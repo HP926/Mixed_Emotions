505f01db2dc5a4ca31b0c68396a4575307595e97
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-07-11 12:06:45 -0600

    soundcloud script2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,19 @@
 //= require turbolinks
 //= require_tree .
 
+
+
 function playSomeMusic(myGenre) {  
-
-
 SC.initialize({      
   client_id: 'fe77db38368f173df942105c06dd7c57'
 });
 
-
-SC.get('/tracks', { genres: myGenre, bpm: { from: 120 } },
+SC.get('/tracks', {genres: myGenre, bpm: { from: 100 } },
   function(tracks) {
     //FIRST soundcloud sends back an array of 50 objects (0~49)
     //so let's randomize which one we play:
       //this logic returns a number between 0 and 50
-      var random = Math.floor(Math.random() * 49);
+      var random = Math.floor(Math.random() * 10);
       var track_url = tracks[random].permalink_url;
       //LAST let's use the oEmbed method to display the soundplayer
       //in the element #target with a options such as auto play
@@ -36,4 +35,19 @@ SC.get('/tracks', { genres: myGenre, bpm: { from: 120 } },
         document.getElementById("target"));
       }
 );
+
+// SC.get('/tracks', { genres: Goodcry, bpm: { from: 60 } },
+//   Goodcry = /tracks/genres(bpm 60)
+//   function(tracks) {
+//     //FIRST soundcloud sends back an array of 50 objects (0~49)
+//     //so let's randomize which one we play:
+//       //this logic returns a number between 0 and 50
+//       var random = Math.floor(Math.random() * 10);
+//       var track_url = tracks[random].permalink_url;
+//       //LAST let's use the oEmbed method to display the soundplayer
+//       //in the element #target with a options such as auto play
+//       SC.oEmbed(track_url, {auto_play: true, color: "ff0066"},
+//         document.getElementById("target"));
+//       }
+// );
 }
